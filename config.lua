610c433b176b0603d625594686a7d85feb0671a6
Marcus <121552016+intexpression@users.noreply.github.com>
2023-06-30 00:16:17 +0200

    xd

---
+++ config.lua
@@ -0,0 +1,34 @@
+Config = {}
+
+Config.EnableJoinTransition = true
+
+--[[
+ 
+ Możesz uzyć identyfikatorów: steam, license(fivem), discord
+ Jak używasz np. steam to nie dajesz steam:333333333 tylko samo -> 333333333    
+  nie potrzebujesz discord: , license: albo steam:
+]]
+
+Config.Team = {
+    {
+        identifierType = 'discord',
+        identifier = '907986608886808596'  
+    },
+    {
+        identifierType = 'discord',
+        identifier = '907986608886808596'
+    },
+    {
+        identifierType = 'discord',
+        identifier = '907986608886808596'
+    },
+    {
+        identifierType = 'discord',
+        identifier = '907986608886808596'
+    }
+
+   -- 4 TO LIMIT NIE DODAWAJ WIECEJ
+}
+
+
+

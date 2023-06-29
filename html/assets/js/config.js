bc6f60d085a0e336aa9c8c32b5d04eee852a5865
Marcus <121552016+intexpression@users.noreply.github.com>
2023-06-30 00:17:22 +0200

    xddd

---
+++ html/assets/js/config.js
@@ -0,0 +1,33 @@
+const config = {
+    Logo: "./assets/img/filled-logo.png", // tu dajesz logo
+    logoStart: 34.5, // START LOGO MOZESZ
+    videoID: "J42fMa-0nO0", // napisz nic aby miec static video  (TE ID BIERZECIE Z YOUTUBE.COM NP: https://www.youtube.com/watch?v={J42fMa-0nO0}
+    defaultVideoVolume: 100,
+    muteVideo: false, // muted video
+    stopVideowhenAudioisMuted: false, // pausuje video jak jest muted
+    enableCursor: true, // wlacza kursor
+    showLinkInfowhileHovering: true, //  pokazuje info jak najedziesz kursorem
+  
+    backgroundImg: "./assets/img/bg.png", // background jak wyjebiesz video youtube
+    servername: "CWELRP.PL",
+    serverdesc: 'Zajebisty serwer duzo bojuwy i kastratuw',
+    socials: [
+        {name: "discord", icon: "./assets/img/media/discord.png", link: "norma.wtf/dc"},
+        {name: "twitch", icon: "./assets/img/media/twitch.png", link: "twitch.tv/24zv"},
+        {name: "web", icon: "./assets/img/media/web.png", link: "norma.wtf"}
+    ],
+    teams: [
+        {name: "cbzc", description: "Właściciel", color: "#ffffff", image: "https://cdn.discordapp.com/attachments/959467970193670164/1013849547295953009/imageedit_3_6111944322.png"},
+        {name: "cbzc", description: "Właściciel", color: "#ffffff", image: "https://cdn.discordapp.com/attachments/959467970193670164/1013849547295953009/imageedit_3_6111944322.png"},
+        {name: "cbzc", description: "Właściciel", color: "#ffffff", image: "https://cdn.discordapp.com/attachments/959467970193670164/1013849547295953009/imageedit_3_6111944322.png"},
+        {name: "cbzc", description: "Właściciel", color: "#ffffff", image: "https://cdn.discordapp.com/attachments/959467970193670164/1013849547295953009/imageedit_3_6111944322.png"},
+    ],
+    changelog: [
+        {title: "# AKTUALIZACJA #", desc: "ZAJEBISTY UPDATE NEW ANTYCHEAT BY CWEL"},
+        {title: "# AKTUALIZACJA #", desc: "ZAJEBISTY UPDATE NEW ANTYCHEAT BY CWEL"},
+        {title: "# AKTUALIZACJA #", desc: "ZAJEBISTY UPDATE NEW ANTYCHEAT BY CWEL"},
+    ],
+
+    customMusic: false, 
+    songDirectory: './assets/music/song.mp3' 
+}
